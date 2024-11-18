688a1cc3cb7111730a403e01b412b67b8926fd17
Daniel <roderm99@gmail.com>
2024-11-17 22:25:19 -0500

    add bouncing down arrow to bottom of hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,10 @@ import {
   scrollSpy,
 } from "react-scroll";
 import HeroBackground from "./HeroBackground";
+import Icon from "@mdi/react";
+import { mdiChevronDown } from "@mdi/js";
+
+<Icon path={mdiChevronDown} size={1} />;
 
 export default function Hero(props) {
   return (
@@ -16,6 +20,19 @@ export default function Hero(props) {
         <h1 className="text-9xl">Dan Roderman</h1>
         <p className="m-5 text-3xl">Full-stack Web Developer</p>
       </div>
+      <Link
+        to="about"
+        smooth={true}
+        duration={1000}
+        className="pointer-events-auto"
+      >
+        <Icon
+          path={mdiChevronDown}
+          size={2}
+          className="absolute bottom-0 left-2/4 -ml-[16px] animate-bounce"
+        />
+      </Link>
+      ;
     </section>
   );
 }
